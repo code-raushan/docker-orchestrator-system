733469846fd324918f899ea29c9765e6f8a9bc69
code-raushan <code-raushan@users.noreply.github.com>
2024-05-13 14:47:11 +0530

    feat: orchestration service created

--- src/service/orchestration.service.ts
+++ src/service/orchestration.service.ts
@@ -1,3 +1,14 @@
-class OrchestrationService {
+import Docker from "dockerode";
 
+class OrchestrationService {
+    private _docker: Docker
+    constructor() {
+        this._docker = new Docker();
+    }
+
+    async create() {
+        const container = await this._docker.createContainer({
+            Image: ''
+        });
+    }
 }
