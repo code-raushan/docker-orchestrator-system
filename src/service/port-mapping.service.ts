c23aa6dfc0cc25a25d837f08dd5ffc783c221af2
code-raushan <code-raushan@users.noreply.github.com>
2024-05-13 16:01:25 +0530

    feat: port mapping service using redis done

---
+++ src/service/port-mapping.service.ts
@@ -0,0 +1,39 @@
+import { BadRequestError } from "../errors/bad-request.error";
+import logger from "../utils/logger";
+import redisClient from "./redis.service";
+
+class PortMapping {
+    private _redisClient;
+    constructor() {
+        this._redisClient = redisClient;
+    }
+
+    async availablePort() {
+        await this._redisClient.connect();
+        for (let i = 8000; i < 9000; i++) {
+            if ((await this._redisClient.hGet("portToContainer", String(i)))) continue;
+            return String(i);
+        }
+        throw new BadRequestError("No available port");
+    }
+
+    async setPortToContainer(containerId: string, availablePort: string) {
+        await this._redisClient.connect();
+        await this._redisClient.hSet("portToContainer", availablePort, containerId).catch(async (err) => {
+            await this._redisClient.disconnect();
+            logger.error(`failed to assign container id to the available port - ${err}`)
+            throw new BadRequestError("failed to assign container id to the available port");
+        });
+    }
+
+    async setContainerToPort(containerId: string, availablePort: string) {
+        await this._redisClient.connect();
+        await this._redisClient.hSet("containerToPort", containerId, availablePort).catch(async (err) => {
+            await this._redisClient.disconnect();
+            logger.error(`failed to assign available port to the container id - ${err}`)
+            throw new BadRequestError("failed to assign available port to the container id");
+        });
+    }
+
+
+}
